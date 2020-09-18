b0ad4b3255555be9ee404ab07207ae16fdca8f2d
angelle rina <angellerinac@gmail.com>
2020-09-18 18:59:08 -0400

    Removed Loader Description

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { motion, useCycle } from 'framer-motion';
+import React from "react";
+import { motion, useCycle } from "framer-motion";
 
 const loaderVariants = {
   animationOne: {
@@ -8,14 +8,14 @@ const loaderVariants = {
     transition: {
       x: {
         yoyo: Infinity,
-        duration: .5
+        duration: 0.5,
       },
       y: {
         yoyo: Infinity,
-        duration: .25,
-        ease: 'easeOut'
-      }
-    }
+        duration: 0.25,
+        ease: "easeOut",
+      },
+    },
   },
   animationTwo: {
     y: [0, -40],
@@ -23,25 +23,25 @@ const loaderVariants = {
     transition: {
       y: {
         yoyo: Infinity,
-        duration: .25,
-        ease: 'easeOut'
-      }
-    }
-  }
-}
+        duration: 0.25,
+        ease: "easeOut",
+      },
+    },
+  },
+};
 
 const Loader = () => {
   const [animation, cycleAnimation] = useCycle("animationOne", "animationTwo");
   return (
     <>
-    <motion.div className="loader"
-      variants={loaderVariants}
-      animate={animation}
-    >
-    </motion.div>
-    <div onClick={() => cycleAnimation()}>Cycle Loader</div>
+      <motion.div
+        className='loader'
+        variants={loaderVariants}
+        animate={animation}
+      ></motion.div>
+      <div onClick={() => cycleAnimation()}></div>
     </>
-  )
-}
+  );
+};
 
 export default Loader;
